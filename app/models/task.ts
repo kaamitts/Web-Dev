815505e0f77cf9be08ebd6354564aac2d26f70f7
kaamitts <156874171+kaamitts@users.noreply.github.com>
2025-03-09 16:00:10 +0500

    Delete app directory

--- app/models/task.ts
+++
@@ -1,8 +0,0 @@
-export interface Task {
-    id: number;
-    title: string;
-    description: string;
-    category: string; // e.g., "Work", "Personal", "Study"
-    status: string;   // e.g., "Pending", "Completed"
-    deadline: Date;
-  }
