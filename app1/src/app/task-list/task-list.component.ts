ad263414468d762e736f9993593371326600ae2f
Kanatbek <atamuratovkanatbek@gmail.com>
2025-03-09 17:07:31 +0500

    Первый коммит в app1

---
+++ app1/src/app/task-list/task-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../services/task.service';
+import { Task } from '../models/task';
+import {TaskItemComponent} from '../task-item/task-item.component';
+
+@Component({
+  selector: 'app-task-list',
+  template: `
+    <h1>Task List</h1>
+    <app-task-item
+      *ngFor="let task of tasks"
+      [task]="task"
+      (markCompleted)="onMarkCompleted($event)">
+    </app-task-item>
+  `,
+  imports: [
+    TaskItemComponent
+  ],
+  styles: [`
+    h1 {
+      padding: 20px 0;
+    }
+  `]
+})
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
+  }
+
+  onMarkCompleted(taskId: number): void {
+    const task = this.tasks.find(t => t.id === taskId);
+    if (task) {
+      task.status = 'Completed';
+    }
+  }
+}
